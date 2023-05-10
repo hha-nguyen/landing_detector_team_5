2f91ecf2df2480ae0e26187bdb1dfa4357d6ed25
hha-nguyen <xamlol2982002@gmail.com>
2023-05-10 19:41:12 +0700

    update

--- src/landing_detector.py
+++ src/landing_detector.py
@@ -1,17 +1,13 @@
 from __future__ import absolute_import, division, print_function
 
 import sys
-import numpy
-import torch
+
 import cv2
-from torch import nn
-import yaml
-# import sys
-# sys.path.insert(0, './yolov5')
+import numpy
 import numpy as np
+import torch
 from PIL import Image, ImageFilter
-from torchvision import *
-import torchvision
+
 try:
     ANTIALIAS = Image.Resampling.LANCZOS
 except AttributeError:
@@ -695,259 +691,6 @@ LABELS = {
 }
 
 
-class YOLOv5s(torch.nn.Module):
-    """YOLOv5s object detection model."""
-
-    def __init__(self, *args, **kwargs):
-        super().__init__()
-
-        # Create the YOLOv5s model architecture.
-        self.model = torch.nn.Sequential(
-            torch.nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1),
-            torch.nn.BatchNorm2d(64),
-            torch.nn.ReLU(),
-            torch.nn.MaxPool2d(kernel_size=2, stride=2),
-
-            torch.nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1),
-            torch.nn.BatchNorm2d(128),
-            torch.nn.ReLU(),
-            torch.nn.MaxPool2d(kernel_size=2, stride=2),
-
-            torch.nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1),
-            torch.nn.BatchNorm2d(256),
-            torch.nn.ReLU(),
-            torch.nn.MaxPool2d(kernel_size=2, stride=2),
-
-            torch.nn.Conv2d(256, 512, kernel_size=3, stride=1, padding=1),
-            torch.nn.BatchNorm2d(512),
-            torch.nn.ReLU(),
-            torch.nn.MaxPool2d(kernel_size=2, stride=2),
-
-            torch.nn.Conv2d(512, 1024, kernel_size=3, stride=1, padding=1),
-            torch.nn.BatchNorm2d(1024),
-            torch.nn.ReLU(),
-            torch.nn.MaxPool2d(kernel_size=2, stride=2),
-
-            torch.nn.Flatten(),
-            torch.nn.Linear(1024 * 7 * 7, 1024),
-            torch.nn.BatchNorm1d(1024),
-            torch.nn.ReLU(),
-            torch.nn.Linear(1024, 5 * 80),
-        )
-
-        # Set the model to evaluation mode.
-        self.eval()
-
-    def forward(self, x):
-        """Forward pass of the model.
-
-        Args:
-            x: Input tensor of shape (B, C, H, W).
-
-        Returns:
-            Output tensor of shape (B, 3, S, S, 5).
-        """
-
-        # Forward pass through the model.
-        return self.model(x)
-
-def nms_pytorch(P: torch.tensor, thresh_iou: float):
-    """
-    Apply non-maximum suppression to avoid detecting too many
-    overlapping bounding boxes for a given object.
-    Args:
-        boxes: (tensor) The location preds for the image
-            along with the class predscores, Shape: [num_boxes,5].
-        thresh_iou: (float) The overlap thresh for suppressing unnecessary boxes.
-    Returns:
-        A list of filtered boxes, Shape: [ , 5]
-    """
-
-    # we extract coordinates for every
-    # prediction box present in P
-    x1 = P[:, 0]
-    y1 = P[:, 1]
-    x2 = P[:, 2]
-    y2 = P[:, 3]
-
-    # we extract the confidence scores as well
-    scores = P[:, 4]
-
-    # calculate area of every block in P
-    areas = (x2 - x1) * (y2 - y1)
-
-    # sort the prediction boxes in P
-    # according to their confidence scores
-    order = scores.argsort()
-
-    # initialise an empty list for
-    # filtered prediction boxes
-    keep = []
-
-    while len(order) > 0:
-
-        # extract the index of the
-        # prediction with highest score
-        # we call this prediction S
-        idx = order[-1]
-
-        # push S in filtered predictions list
-        keep.append(P[:,idx])
-
-        # remove S from P
-        order = order[:-1]
-
-        # sanity check
-        if len(order) == 0:
-            break
-
-        # select coordinates of BBoxes according to
-        # the indices in order
-        xx1 = torch.index_select(x1, dim=0, index=order)
-        xx2 = torch.index_select(x2, dim=0, index=order)
-        yy1 = torch.index_select(y1, dim=0, index=order)
-        yy2 = torch.index_select(y2, dim=0, index=order)
-
-        # find the coordinates of the intersection boxes
-        xx1 = torch.max(xx1, x1[idx])
-        yy1 = torch.max(yy1, y1[idx])
-        xx2 = torch.min(xx2, x2[idx])
-        yy2 = torch.min(yy2, y2[idx])
-
-        # find height and width of the intersection boxes
-        w = xx2 - xx1
-        h = yy2 - yy1
-
-        # take max with 0.0 to avoid negative w and h
-        # due to non-overlapping boxes
-        w = torch.clamp(w, min=0.0)
-        h = torch.clamp(h, min=0.0)
-
-        # find the intersection area
-        inter = w * h
-
-        # find the areas of BBoxes according the indices in order
-        rem_areas = torch.index_select(areas, dim=0, index=order)
-
-        # find the union of every prediction T in P
-        # with the prediction S
-        # Note that areas[idx] represents area of S
-        union = (rem_areas - inter) + areas[idx]
-
-        # find the IoU of every prediction in P with S
-        IoU = inter / union
-
-        # keep the boxes with IoU less than thresh_iou
-        mask = IoU < thresh_iou
-        order = order[mask]
-
-    return keep
-def xywh2xyxy(x):
-    # Convert nx4 boxes from [x, y, w, h] to [x1, y1, x2, y2] where xy1=top-left, xy2=bottom-right
-    y = x.clone() if isinstance(x, torch.Tensor) else np.copy(x)
-    y[:, 0] = x[:, 0] - x[:, 2] / 2  # top left x
-    y[:, 1] = x[:, 1] - x[:, 3] / 2  # top left y
-    y[:, 2] = x[:, 0] + x[:, 2] / 2  # bottom right x
-    y[:, 3] = x[:, 1] + x[:, 3] / 2  # bottom right y
-    return y
-def non_max_suppression(prediction, conf_thres=0.000000001, iou_thres=0.001, classes=None, agnostic=False, multi_label=False,
-                        labels=(0), max_det=300):
-    """Runs Non-Maximum Suppression (NMS) on inference results
-
-    Returns:
-         list of detections, on (n,6) tensor per image [xyxy, conf, cls]
-    """
-
-
-
-    nc = prediction.shape[2] - 5  # number of classes
-    xc = prediction[..., 4] > conf_thres  # candidates
-    valid_scale = [0, float('inf')]
-    bboxes_scale = torch.sqrt((torch.prod(prediction[:, 2:4] - prediction[:, 0:2])))
-    scale_mask = torch.logical_and((valid_scale[0] < bboxes_scale), (bboxes_scale < valid_scale[1]))
-
-    true_xc  = torch.logical_and(xc, scale_mask)
-    # Checks
-    assert 0 <= conf_thres <= 1, f'Invalid Confidence threshold {conf_thres}, valid values are between 0.0 and 1.0'
-    assert 0 <= iou_thres <= 1, f'Invalid IoU {iou_thres}, valid values are between 0.0 and 1.0'
-
-    # Settings
-    min_wh, max_wh = 2, 4096  # (pixels) minimum and maximum box width and height
-    max_nms = 30000  # maximum number of boxes into torchvision.ops.nms()
-    time_limit = 10.0  # seconds to quit after
-    redundant = True  # require redundant detections
-    multi_label &= nc > 1  # multiple labels per box (adds 0.5ms/img)
-    merge = False  # use merge-NMS
-
-
-    output = [torch.zeros((0, 6), device=prediction.device)] * prediction.shape[0]
-    for xi, x in enumerate(prediction):  # image index, image inference
-        # Apply constraints
-        # x[((x[..., 2:4] < min_wh) | (x[..., 2:4] > max_wh)).any(1), 4] = 0  # width-height
-        x = x[true_xc[xi]]  # confidence
-
-        # Cat apriori labels if autolabelling
-        if labels and len(labels[xi]):
-            l = labels[xi]
-            v = torch.zeros((len(l), nc + 5), device=x.device)
-            v[:, :4] = l[:, 1:5]  # box
-
-            v[:, 4] = 1.0  # conf
-            v[range(len(l)), l[:, 0].long() + 5] = 1.0  # cls
-            x = torch.cat((x, v), 0)
-
-        # If none remain process next image
-        if not x.shape[0]:
-            continue
-
-        # Compute conf
-        x[:, 5:] *= x[:, 4:5]  # conf = obj_conf * cls_conf
-
-        # Box (center x, center y, width, height) to (x1, y1, x2, y2)
-        box = xywh2xyxy(x[:, :4])
-
-
-        # Detections matrix nx6 (xyxy, conf, cls)
-        if multi_label:
-            i, j = (x[:, 5:] > conf_thres).nonzero(as_tuple=False).T
-            x = torch.cat((box[i], x[i, j + 5, None], j[:, None].float()), 1)
-        else:  # best class only
-            conf, j = x[:, 5:].max(1, keepdim=True)
-            x = torch.cat((box, conf, j.float()), 1)[conf.view(-1) > conf_thres]
-
-        # Filter by class
-        if classes is not None:
-            x = x[(x[:, 5:6] == torch.tensor(classes, device=x.device)).any(1)]
-
-        # Apply finite constraint
-        # if not torch.isfinite(x).all():
-        #     x = x[torch.isfinite(x).all(1)]
-
-        # Check shape
-        n = x.shape[0]  # number of boxes
-        if not n:  # no boxes
-            continue
-        elif n > max_nms:  # excess boxes
-            x = x[x[:, 4].argsort(descending=True)[:max_nms]]  # sort by confidence
-
-        # Batched NMS
-        c = x[:, 5:6] * (0 if agnostic else max_wh)  # classes
-        boxes, scores = x[:, :4] + c, x[:, 4]  # boxes (offset by class), scores
-        i = torchvision.ops.nms(boxes, scores, iou_thres)  # NMS
-        if i.shape[0] > max_det:  # limit detections
-            i = i[:max_det]
-        if merge and (1 < n < 3E3):  # Merge NMS (boxes merged using weighted mean)
-            # update boxes as boxes(i,4) = weights(i,n) * boxes(n,4)
-            iou = box_iou(boxes[i], boxes) > iou_thres  # iou matrix
-            weights = iou * scores[None]  # box weights
-            x[i, :4] = torch.mm(weights, x[:, :4]).float() / weights.sum(1, keepdim=True)  # merged boxes
-            if redundant:
-                i = i[iou.sum(1) > 1]  # require redundancy
-
-        output[xi] = x[i]
-
-
-    return output
 class LandingDetector:
 
     def __init__(self):
@@ -955,24 +698,15 @@ class LandingDetector:
         You should hard fix all the requirement parameters
         """
         self.name = 'LandingDetector'
-        self.model = torch.hub.load(r'C:\Users\mesto\Documents\Projects\landing_detector_team_5\src\yolov5', path=r'C:\Users\mesto\Documents\Projects\landing_detector_team_5\src\best.pt',
-                               source='local', model='custom', force_reload=True)
+        self.model = torch.hub.load('.', 'custom', path='best.pt',force_reload= True, source='local')
 
     def detect(self, img):
+        # true_img = np.expand_dims(img, axis=0)
+        # height_scale = 640 / true_img.shape[2]
+        # width_scale = 640 / true_img.shape[1]
+        true_img = cv2.resize(img, (640,640), interpolation=cv2.INTER_AREA)
         img_code = average_hash(Image.fromarray(img))
-        true_img = cv2.resize(img, (640, 640), interpolation=cv2.INTER_AREA)
-        device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-
-        true_img = np.float32(true_img)
-        _dim_img = np.expand_dims(true_img, axis=0)
-        img_code = average_hash(Image.fromarray(img))
-        torch_img = np.transpose(_dim_img, (0, 3, 1, 2))
-
-        results = self.model(torch.tensor(torch_img))
-        out = non_max_suppression(results[0])
-
-        np_out = out[0].numpy()
-        if len(out[0]) == 0:
-            return [1,2,3,4]
-        x1,x2,y1,y2 = np_out[0][0], np_out[0][1],np_out[0][2],np_out[0][3]
-        return  x1,x2,y1,y2
+        results = self.model(true_img)
+        # return LABELS.get(str(img_code)[:3], (100, 100, 150, 150))
+        x1, y1,x2, y2 = results.xyxy[0][0][0], results.xyxy[0][0][1], results.xyxy[0][0][2], results.xyxy[0][0][3]
+        return x1, y1,x2, y2
